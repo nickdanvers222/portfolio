355e9b2453d56b8e174d7bed0be39cb5254d6eb9
Ravenor222 <ndanvers222@gmail.com>
2020-04-19 13:22:32 -0700

    import bootstrap, temp cover found

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from '../logo.svg';
+import mtn from '../synthwave.jpg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-div">
+        <img src={mtn} className="App-logo" alt="logo" />
+
+      </div>
+
     </div>
   );
 }
