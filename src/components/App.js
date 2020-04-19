1abbd3265fb0602f1849bcc5c104c44d0a704f6f
Ravenor222 <ndanvers222@gmail.com>
2020-04-19 14:18:01 -0700

    brought in navbar, bringing in carousel

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import mtn from '../synthwave.jpg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className="App">
-      <div className="main-div">
-        <img src={mtn} className="App-logo" alt="logo" />
+    <Navbar bg="dark" variant='dark' expand="lg">
+      <Navbar.Brand href="#home">Nicholas Danvers</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+     <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Projects</Nav.Link>
+      <Nav.Link href="#link">Interests</Nav.Link>
+      <Nav.Link href="#link">Posts</Nav.Link>
+      <Nav.Link href="#link">Contact me</Nav.Link>
+     </Nav>
+     </Navbar.Collapse>
+    </Navbar>
+    <div className="main-div">
+      <img src={mtn} className="App-logo" alt="logo" />
 
       </div>
 
