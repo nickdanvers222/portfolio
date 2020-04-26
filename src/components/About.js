284f24aec1f53eaf7755c70f65f1aac63c8ee852
Ravenor222 <ndanvers222@gmail.com>
2020-04-25 23:18:47 -0700

    rough about me description draft

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,8 @@ const About = () => {
         <div id='about' className='aboutme-container'>
             <h2 className='aboutme-title'>About me</h2>
             <div className='aboutme-image'></div>
-            <p className='aboutme-description'>I'm just an amicable fellow, there are some words here but they don't actual amount to much as I am just trying to take up as much space as possible. Did you know that the fourth Earl of Northumbria had three thumbs? Me neither!</p>
+            <p className='aboutme-description'>I'm a full-stack developer and React JS / Native enthusiast with a passion for designing web, mobile & tablet interfaces.  </p>
+            <p className='aboutme-description'>After spending nearly a decade working in the heat of the kitchen, my passion for programming sent me to Lighthouse Labs: A program full of challenges, twists and turns due to the Coronavirus, and fantastic learning experiences. I hope to bring my years of experience in a service-oriented role and the resulting ability to work under any conditions wherever I code </p>
         </div>
     )
 
